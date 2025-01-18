5a0377c616cca54c1821f5031d8582977ee071ce
Daniel Filipkowski <107939577+DJFilipkowski0131@users.noreply.github.com>
2025-01-17 22:07:21 -0500

    update for YT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,8 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    ReactiveFormsModule
-  ],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
